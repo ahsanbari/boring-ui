4fd21853630cca3a0e371633abb7ab2cd8f8908d
Ahsan <bariahsan786@gmail.com>
2023-03-24 15:42:04 -0700

    Set up TS and ESLint

---
+++ src/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const firstWords = "hello world";
+console.info(firstWords);
+const test = 0;
+console.log(test);
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+const firstWords:string = "hello world"
+console.info(firstWords);
+
+const test: number = 0;
+
+console.log(test);
+
