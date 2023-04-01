7be3cc769cf13c65fb7aa8b02a0e8a721629e7ff
Ahsan <bariahsan786@gmail.com>
2023-04-01 18:57:28 -0400

    updated design tokens + button type styles

--- Boring-UI/src/components/boringButton.ts
+++ Boring-UI/src/components/boringButton.ts
@@ -1,63 +1,89 @@
-export enum buttonProps {
-    color = 'color',
-    backgroundColor = 'background-color',
-    width = 'width',
-    height = 'height',
-    displayOrder = 'display-order',
+import { designTokens } from "../tokens";
 
-}
+
+export enum ButtonType {
+    Default = 'default',
+    secondary = 'secondary',
+    Ghost = 'ghost',
+  }
 
 export class BoringButton extends HTMLElement {
-    public buttonStyle?: string;
 
+    public onclick: ((this: GlobalEventHandlers, ev: MouseEvent) => any) | null = null;
+  
     constructor() {
       super();
-      
-        this.buttonStyle = `button {
-            display: flex;
-            background-color: ${buttonProps.backgroundColor ?? 'purple'};
-            color: ${buttonProps.backgroundColor ?? 'white'};
-            width: ${buttonProps.width};
-            height: ${buttonProps.height};
-            justify-content: center;
-            align-items: ${this.getDisplayOrder(buttonProps.displayOrder)};
-        }`;
-
-    }
-
-    connectedCallback() {
-        this.renderButton();
-    }
-
-    private renderButton(): void {
-        const shadow = this.attachShadow({ mode: 'open' });
-
-        const button = document.createElement('button');
-        shadow.appendChild(button);
-
-        const slot = document.createElement('slot');
-        slot.textContent = 'Nothing is slotted';
-        button.appendChild(slot);
-
-        const style = document.createElement('style');
-        style.textContent = this.buttonStyle ?? '';
-        shadow.appendChild(style);
-    }
-
-
-    getDisplayOrder(buttonProp: string): string {
-
-        switch(buttonProp) {
-            case('left'):
-                return 'flex-start';
-            case('right'):
-                return 'flex-end';
-            default:
-                return 'center';
+      const template = document.createElement('template');
+      template.innerHTML = `
+      <style>
+        :host {
+            display: inline-block;
         }
+
+        button {
+            
+            padding: 0.5rem 1rem;
+            border: none;
+            cursor: pointer;
+
+            border-radius: ${designTokens.borderRadius.button};
+            font-family: ${designTokens.text.fontFamily.family};
+            font-weight: ${designTokens.text.fontWeight.Regular};
+            font-size: ${designTokens.text.textSize.body};
+            transition: ${designTokens.transition.transitionSpeed.fast}
+        }
+
+        :host([type="default"]) button {
+            background-color: ${designTokens.backgroundColor.default.primary};
+            color: ${designTokens.text.textColor.primary};
+        }
+        :host([type="default"]) button:hover {
+            background-color: ${designTokens.backgroundColor.default.secondary};
+        }
+        :host([type="default"]) button:active {
+            background-color: ${designTokens.backgroundColor.default.tertiary};
+        }
+
+
+        :host([type="secondary"]) button {
+            background-color: ${designTokens.backgroundColor.secondary.primary};
+            color: ${designTokens.text.textColor.secondary};
+            border: 2px solid ${designTokens.backgroundColor.default.primary};
+        }
+        :host([type="secondary"]) button:hover {
+            background-color: ${designTokens.backgroundColor.secondary.secondary};
+            border: 2px solid ${designTokens.backgroundColor.default.secondary};
+        }
+        :host([type="secondary"]) button:active {
+            background-color: ${designTokens.backgroundColor.secondary.disabled};
+            border: 2px solid ${designTokens.backgroundColor.default.disabled};
+        }
+
+        :host([type="ghost"]) button {
+            background-color: ${designTokens.backgroundColor.ghost.primary};
+            color: ${designTokens.text.textColor.secondary};
+            border: none;
+        }
+        :host([type="ghost"]) button:hover {
+            background-color: ${designTokens.backgroundColor.ghost.secondary};
+        }
+        :host([type="ghost"]) button:active {
+            background-color: ${designTokens.backgroundColor.ghost.disabled};
+        }
+        </style>
+        <button onclick=${this.onclick ?? this.defaultClickEvent()}>
+            <slot></slot>
+        </button>
+      `;
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
+    public defaultClickEvent():void {
+        console.log('made it');
+    }
 
-}
+  }
+  
 
-
+  
---
+++ Boring-UI/src/tokens.ts
@@ -0,0 +1,65 @@
+export const designTokens = {
+    backgroundColor: {
+        default: {
+            primary: '#4D11F3',
+            secondary: '#3409AC',
+            tertiary: '#26077E',
+            disabled: '#4D11F3',
+        },
+        secondary: {
+            primary: 'transparent',
+            secondary: '#F1F1F4',
+            disabled: '#E5E5EA',
+        },
+        ghost: {
+            primary: 'transparent',
+            secondary: '#F8F8F9',
+            disabled: '#F1F1F4',
+        },
+    },
+    
+    borderRadius: {
+        button: '8px',
+    },
+    text: {
+        fontFamily: { family: 'Inter' },
+        fontWeight: {
+            Thin: 100,
+            ExtraLight: 200,
+            Light: 300, 
+            Regular: 400,
+            Medium: 500,
+            SemiBold: 600,
+            Bold: 700,
+            ExtraBold: 800,
+        },
+        textColor: {
+            primary: '#FFFFFF',
+            secondary: '#373745',
+        },
+        textSize: {
+            h1: '5rem',
+            h2: '4rem',
+            h3: '3rem',
+            h4: '2.5rem',
+            h5: '2rem',
+            body2xl: '1.5rem',
+            bodyxl: '1.25rem',
+            body: '1rem',
+            bodymd: '0.75rem',
+            bodysml: '0.5rem',
+
+        }
+    },
+
+    transition: {
+        transitionSpeed: {
+            faster: '0.15s all ease-in-out',
+            fast: '0.2s all ease-in-out',
+            normal: '0.33s all ease-in-out',
+            slow: '0.5s all ease-in-out',
+        }
+    }
+
+
+}
